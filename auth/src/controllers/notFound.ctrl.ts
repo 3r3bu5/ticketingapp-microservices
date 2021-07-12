a3d4d0669070429f8b4beb9d01f3c6ad4a94395f
Ashraf Gehad <deva4rf@gmail.com>
2021-07-12 15:57:21 +0200

    feat: add express-async-errors package to handle async errors

--- auth/src/controllers/notFound.ctrl.ts
+++ auth/src/controllers/notFound.ctrl.ts
@@ -1,7 +1,7 @@
 import {NextFunction, Request, Response} from 'express'
 import { notFoundError } from '../error/notFoundError';
 
- const notFoundCtrl = (req:Request, res:Response, next:NextFunction) => {
+ const notFoundCtrl = async (req:Request, res:Response, next:NextFunction) => {
     throw new notFoundError()
 };
 

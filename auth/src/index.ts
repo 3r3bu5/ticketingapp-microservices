978d192d0a6fc3359087875ccf3d7c0f3a0e0af4
Ashraf Gehad <deva4rf@gmail.com>
2021-07-14 12:16:27 +0200

    refact: refactor index.ts to make the app ready for testing

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,47 +1,6 @@
-import express from 'express';
-import 'express-async-errors'
-import { json } from 'body-parser';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { currentUserRouter } from './routes/current-user';
-import { errorHandler } from './middleware/error-handler';
-import { notFoundRouter } from './routes/404';
 import { connectDB } from './config/db.config';
-import passport from 'passport';
-import cookieSession from 'cookie-session';
-import './config/passport'
+import {app} from './app'
 
-
-
-const app = express();
-app.set('trust proxy', 1) 
-app.use(json());
-
-//cookie-session
-app.use(cookieSession({
-  signed: false,
-  secure: true
-}))
-
-// passport init 
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Routes
-app.use(signinRouter)
-app.use(signoutRouter)
-app.use(signupRouter)
-app.use(currentUserRouter)
-
-// 404 Route
-app.use(notFoundRouter)
-// Error handler 
-app.use(errorHandler)
-
-if (!process.env.JWT_KEY){
-  throw new Error("JWT key must be defined")
-}
 connectDB()
 
 app.listen(4000, () => {

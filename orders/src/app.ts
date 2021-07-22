904a7c46e8063d670b504536de14e9457d1c821b
Ashraf Gehad <deva4rf@gmail.com>
2021-07-23 00:10:44 +0200

    feat: Initiate the orders service

---
+++ orders/src/app.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import { errorHandler, currentUser } from '@a4hticket/common';
+import { notFoundRouter } from './routes/404';
+import cookieSession from 'cookie-session';
+
+const app = express();
+app.set('trust proxy', 1);
+app.use(json());
+
+//cookie-session
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test' ? true : false
+  })
+);
+
+app.use(currentUser as any);
+
+// Routes
+
+
+// 404 Route
+app.use(notFoundRouter);
+// Error handler
+app.use(errorHandler as any);
+
+export { app };

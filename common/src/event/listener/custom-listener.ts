6c34c9d130f55bfec83e42f78dd77bdeb6d6afc5
Ashraf Gehad <deva4rf@gmail.com>
2021-07-23 14:00:52 +0200

    update: update base listener inside common package

--- common/src/event/listener/custom-listener.ts
+++ common/src/event/listener/custom-listener.ts
@@ -1,46 +1,50 @@
-import { Message, Stan} from 'node-nats-streaming'
-import {Subjects} from '../const/subjects'
+import { Message, Stan } from 'node-nats-streaming';
+import { Subjects } from '../const/subjects';
 
 interface Event {
-    subject: Subjects,
-    data: any
+  subject: Subjects;
+  data: any;
 }
 
 export abstract class Listener<T extends Event> {
+  abstract subject: T['subject'];
+  abstract queueGroupName: string;
+  abstract onMessage(data: T['data'], msg: Message): void;
+  private client: Stan;
+  protected ackWait = 5 * 1000;
 
-    abstract onMessage(data:T['data'], msg: Message) : void;
-    abstract queueGroupName:string;
-    abstract subject:T['subject'];
-    private client:Stan;
-    protected ackWait = 5 * 1000
+  constructor(client: Stan) {
+    this.client = client;
+  }
 
-    constructor (client: Stan){
-        this.client = client
-    }
-    options = () => {
-        return this.client
-        .subscriptionOptions()
-        .setManualAckMode(true)
-        .setDeliverAllAvailable()
-        .setDurableName(this.queueGroupName)
-        .setAckWait(this.ackWait)
-    }
-    listen() {
-        const subscription = this.client.subscribe(this.subject, this.queueGroupName, this.options())
-        
-        subscription.on('message', (msg: Message) => {
-            console.log(
-                `Message Recieved: ${this.subject} | ${this.queueGroupName}`
-            )
-        const parsedData = this.parseData(msg)
-        this.onMessage(parsedData, msg)
-        })
+  subscriptionOptions() {
+    return this.client
+      .subscriptionOptions()
+      .setDeliverAllAvailable()
+      .setManualAckMode(true)
+      .setAckWait(this.ackWait)
+      .setDurableName(this.queueGroupName);
+  }
 
-    }
-    parseData(msg: Message) {
-        const data = msg.getData()
-        return typeof data === 'string'
-        ? JSON.parse(data)
-        : JSON.parse(data.toString('utf-8'))
-    }
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on('message', (msg: Message) => {
+      console.log(`Message received: ${this.subject} / ${this.queueGroupName}`);
+
+      const parsedData = this.parseMessage(msg);
+      this.onMessage(parsedData, msg);
+    });
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData();
+    return typeof data === 'string'
+      ? JSON.parse(data)
+      : JSON.parse(data.toString('utf8'));
+  }
 }
